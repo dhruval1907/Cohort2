d9a4fa4ddb69f8a306a97bc0885d40d361fc760b
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-10-07 08:21:57 +0530

    first commit

---
+++ CSS3/Home_page/script.js
@@ -0,0 +1,15 @@
+var tl = gsap.timeline()
+
+
+tl.from("#nav-part1 a",{
+    opacity:0,
+    y:20,
+    duartion:3,
+    stagger:0.2,
+})
+
+tl.from("#nav-part1 h3",{
+    opacity:0,
+    y:20,
+    duartion:3
+})
