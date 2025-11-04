a33ed702a50175ce7659a8030aad0403dd8cb832
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-04 23:06:58 +0530

    table

--- js/loops_condition/script.js
+++ js/loops_condition/script.js
@@ -1,27 +1,36 @@
 // 1.  ASK USER AGE ELIGIBLE FOR VOTE OR NOT 
 // ERROR HANDLING
 
-let age = prompt("enter your age : ");
+// let age = prompt("enter your age : ");
 
-// console.log(typeof(age)); 
-if (age === null) {
-    console.error("you cancelled it !");
-} else {
-    if (age.trim() === "") {
-        console.error("please provide me a number ");
-    }
-    else {
-        age = Number(age)
-        if (isNaN(age)) {
-            console.warn("please enter a number")
-        }
-        else {
-            console.log(`your age is ${age}`);
+// // console.log(typeof(age)); 
+// if (age === null) {
+//     console.error("you cancelled it !");
+// } else {
+//     if (age.trim() === "") {
+//         console.error("please provide me a number ");
+//     }
+//     else {
+//         age = Number(age)
+//         if (isNaN(age)) {
+//             console.warn("please enter a number")
+//         }
+//         else {
+//             console.log(`your age is ${age}`);
 
-            if (age >= 18) console.log("you can vote");
-            else console.log("you are not eligible for vote")
-        }
-    }
+//             if (age >= 18) console.log("you can vote");
+//             else console.log("you are not eligible for vote")
+//         }
+//     }
+// }
+
+
+// table of number 
+// let 
+let n = prompt("enter a number ")
+
+for(i=0;i<=10;i++){
+    let number = n * i;
+    console.log(`${n} x ${i}  =  ${number}`);
 }
-
 
