f6191015bb0cb937b51924980bcc349272d5edad
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-04 22:10:51 +0530

    addfileandsolveq1

---
+++ js/loops_condition/script.js
@@ -0,0 +1,9 @@
+// 1.  ASK USER AGE ELIGIBLE FOR VOTE OR NOT 
+// EXCEPTION HANDLING
+
+let age = prompt("enter your age : ");
+
+
+
+if(age>=18) console.log("you can vote");
+else console.log("you are not eligible for vote")
