160cc9e31dcc6f7349914f6c370ff323fbd70a5b
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-06 15:46:59 +0530

    passwordchecker

--- js/loops_condition/script.js
+++ js/loops_condition/script.js
@@ -45,3 +45,45 @@
 // }
 
 // 4. ask user for password is matched print match
+
+// let sahipassword = "kese ho"
+
+// let password = prompt("password batao  ")
+
+// if(password === null){
+//     console.error("entered something");
+// }
+// else{
+//     if(password.trim() === ""){
+//         console.error("entered a valid password");
+//     }
+//     else{
+//         if(password === sahipassword){
+//             console.log("matched");
+//         }    
+//         else{
+//             console.log("not matched");
+//         }
+//     }
+// }
+
+// 5. allow only 3 attempts for correct password if user enter after three then account locked 
+let right = "hello";
+let attempt = 0;
+let flag = false
+
+let password = prompt("passowrd batao")
+attempt++;
+if(password == right) flag = true
+
+while(password !== right){
+    if(attempt == 3) {
+        console.error("account locked");
+        break;
+    }
+    password = prompt("passowrd batao")
+    if(password == right) flag = true
+    attempt++;
+}
+
+if(flag == true) console.log("account opened");
