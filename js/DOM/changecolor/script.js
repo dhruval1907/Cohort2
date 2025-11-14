4c4ef50370d56a699ee931aa182672398c83b644
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-14 22:05:59 +0530

    stattingdom

---
+++ js/DOM/changecolor/script.js
@@ -0,0 +1,12 @@
+let box = document.querySelector("#box")
+let btn = document.querySelector("button")
+
+
+btn.addEventListener("click",function(){
+    // console.log("hello");
+    var c1 = Math.floor(Math.random()*255)
+    var c2 = Math.floor(Math.random()*255)
+    var c3 = Math.floor(Math.random()*255)
+
+    box.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+})
