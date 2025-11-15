3db80f3d498ebed43ebe40295d5960d2582a847d
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-15 13:56:35 +0530

    domcolor

--- js/DOM/changecolor/script.js
+++ js/DOM/changecolor/script.js
@@ -1,12 +1,18 @@
 let box = document.querySelector("#box")
 let btn = document.querySelector("button")
 
+btn.addEventListener("click", function () {
 
-btn.addEventListener("click",function(){
-    // console.log("hello");
-    var c1 = Math.floor(Math.random()*255)
-    var c2 = Math.floor(Math.random()*255)
-    var c3 = Math.floor(Math.random()*255)
+    let c1_1 = Math.floor(Math.random() * 255)
+    let c1_2 = Math.floor(Math.random() * 255)
+    let c1_3 = Math.floor(Math.random() * 255)
 
-    box.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+    let c2_1 = Math.floor(Math.random() * 255)
+    let c2_2 = Math.floor(Math.random() * 255)
+    let c2_3 = Math.floor(Math.random() * 255)
+
+    box.style.background = `linear-gradient(135deg, 
+            rgb(${c1_1},${c1_2},${c1_3}), 
+            rgb(${c2_1},${c2_2},${c2_3})
+        )`
 })
