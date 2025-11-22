8742716be5f9c9e9de51688f6dd1f14706a9cc2f
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-22 21:35:34 +0530

    dommmmm

--- js/DOM/bsics/script.js
+++ js/DOM/bsics/script.js
@@ -1,15 +1,15 @@
 let h1 = document.querySelector('h1')
-let main = document.querySelector("main")
+let btn1 = document.querySelector('#btn1')
+let btn2 = document.querySelector('#btn2')
 
-h1.addEventListener("click", function () {
-    h1.style.backgroundColor = "red"
-    h1.style.color = "lightblue"
-    h1.innerHTML = "i am batman"
+let counter = 0;
+
+btn1.addEventListener("click", function () {
+    counter++;
+    h1.innerHTML = counter
 })
 
-main.addEventListener("mousemove", function () {
-    h1.style.backgroundColor = "white"
-    h1.style.color = "red"
-    h1.innerHTML = "kese ho bhhai"
+btn2.addEventListener("click", function () {
+    counter--;
+    h1.innerHTML = counter
 })
-
