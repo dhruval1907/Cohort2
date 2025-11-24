aec91c5a9592a59880b4c21b4ef59c0a1e137292
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-24 14:06:50 +0530

    addingshit

---
+++ js/DOM/iplteam/script.js
@@ -0,0 +1,23 @@
+var main = document.querySelector("main")
+var h1 = document.querySelector("h1")
+var center = document.querySelector(".center")
+var btn = document.querySelector("button")
+var img = document.querySelector(".img")
+var arr = [
+    {
+        team : "RCb",
+        winner : "https://imgs.search.brave.com/eU96iUmrhOZ7FFNEc6p87YO9rvreZA681bSy4QUGgxQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2UxLzJk/LzA4L2UxMmQwOGFl/ODM4MzQ3YmQ5MWE4/NjgxMDdhYjhhYWRh/LmpwZw"
+    },
+    {
+        team : "CSk",
+        winner : "https://imgs.search.brave.com/HQfuRDu2phiXjs4btxbNU6JRtL688tU81gB-QS-rbUQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMy53/cC5jb20vaW1hZ2Vz/LnRoZXF1aW50LmNv/bS90aGVxdWludC8y/MDIzLTA1LzNlN2Rl/YWViLWUwNDYtNGIx/ZS1hN2U4LTQ4YTdk/NzZlZGQ0OS9TUENT/NTY1Ny5KUEc_c3Ry/aXA9YWxs" 
+    },
+    {
+        team : "SRh",
+        winner : "https://imgs.search.brave.com/7f2nYWne4sxD1Rc6iJuAE1a_VYTjYunmjKMwOh9ppHU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Y3JpY2tldHdpbm5l/ci5jb20vX25leHQv/aW1hZ2UvP3VybD1o/dHRwczovL21lZGlh/LmNyaWNrZXR3aW5u/ZXIuY29tL21lZGlh/LzIwMjUvMDMvR211/dzVCZ2JBQUF3aHlE/LndlYnAmdz0xMjAw/JnE9ODU"
+    },
+    {
+        team : "KKR",
+        winner : "https://imgs.search.brave.com/vFi3Q5qYjP5u0WJlgkCGXruJ7nX_TTKM8_9a6NPLs78/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuaW5kaWFuZXhw/cmVzcy5jb20vMjAy/NC8wNS9TaHJleWFz/LUl5ZXItS0tSLUlQ/TC0yMDI0LUtLUi12/cy1TUkguanBnP3c9/NjQw"
+    }
+]
