c715ed353835ed7f180ef93a09b47e5582c8a306
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-24 18:57:08 +0530

    finishedd

--- js/DOM/spotify/script.js
+++ js/DOM/spotify/script.js
@@ -1,24 +1,17 @@
-// Ek hi music player banate hain
 var audio = new Audio();
 
-// Sab button ko select kar lo
 var buttons = document.querySelectorAll("button");
 
-// Har button par click event lagate hain
 buttons.forEach(function (btn) {
 
     btn.addEventListener("click", function () {
 
-        // Button me data-song attribute se mp3 ka path milega
         var song = btn.getAttribute("data-song");
 
-        // Agar is button me song na ho toh kuch mat karo
         if (!song) return;
 
-        // Pehle audio src set karo
         audio.src = song;
 
-        // Fir play karo
         audio.play();
 
         console.log("Playing:", song);
