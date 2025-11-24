c300a82a315c1a32decb9c2a9c80a8f3f410ae15
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-24 16:00:33 +0530

    addinginspotify

---
+++ js/DOM/spotify/script.js
@@ -0,0 +1,10 @@
+var btn1 = document.querySelector(".aujla")
+var btn2 = document.querySelector(".sidhu")
+var btn3 = document.querySelector(".kalli")
+var btn4 = document.querySelector(".arjit")
+var btn5 = document.querySelector(".top")
+
+// console.log(btn1,btn2,btn3,btn4,btn5);
+
+
+
