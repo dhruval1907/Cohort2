487cb272a1e48a0383cea93053659f0c651b9d4b
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-07 10:06:04 +0530

    oopssssssss

--- oopsbegins/basics/script.js
+++ oopsbegins/basics/script.js
@@ -6,10 +6,21 @@ class CreatePencil {
         this.color = color;
     }
 
+    erase() {
+        document.body.querySelectorAll("h1").forEach((elem)=> {
+            if (elem.style.color === this.color) {
+                elem.remove()
+            }
+        })
+    }
+
     write(text) {
         let h1 = document.createElement("h1")
         h1.textContent = text;
         h1.style.color = this.color
+        document.body.appendChild(h1)
+    }
 }
 
 let pencil1 = new CreatePencil("natrajpencil", "sheryians", "123", "red")
+let pencil2 = new CreatePencil("natrajpencil", "sheryians", "123", "blue")
