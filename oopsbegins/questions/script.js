fbaa378ed1e6b5fa1189cfbc9898ba0d4dfd6f5c
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-07 11:54:48 +0530

    oopssqustionsssssssss

--- oopsbegins/questions/script.js
+++ oopsbegins/questions/script.js
@@ -25,4 +25,49 @@
 // let u2 =new Users("dhruvi","hdhi@gmail.com")
 // let u3 =new Users("hasrh","hdhi@gmail.com")
 
-//
+//create product obj stores name,price and rerurn final discount
+
+// let prt = {
+//     pname : "cap",
+//     price :3300,
+//     discontPrice : function(){
+//        return  this.price - 200;
+//     }
+
+    
+// }
+
+// console.log(prt.discontPrice())
+
+// create a class with the speeed and   brand
+
+// class Car{
+//     constructor(speed,brand){
+//         this.speed = speed
+//         this.brand = brand
+//     }
+
+//     showCar(){
+//         console.log(this.speed + "--- " + this.brand);
+//     }
+// }
+
+// let c1 = new Car(120,"maruti")
+
+// create s student class nd that accept the name and roll num
+
+// class Students{
+//     constructor(name,rno){
+//         this.name = name;
+//         this.rno = rno
+//     }
+
+//     introduce(){
+//         return this.name  + "- "+ this.rno
+//     }
+// }
+
+// let st1 = new Students("dhruval",1234)
+
+
+// create a function and set the this valeu usig the bind
