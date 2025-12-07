39568f3daa5db5b878f2663427afd36e5a66c4e7
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-07 11:25:48 +0530

    oopssqustionsssssssss

--- oopsbegins/questions/script.js
+++ oopsbegins/questions/script.js
@@ -1,5 +1,4 @@
 // object creattion and get the function
-
 // let user = {
 //     name: "dhruval",
 //     email: "dhruvaldalwadi1907@gmail.com",
@@ -10,3 +9,20 @@
 // }
 // user.loggedIn()
 
+// 5 object creation with classsssssssssssss
+// class Users{
+//     constructor(name,email){
+//         this.name = name
+//         this.email = email
+//     }
+
+//     loggedIn(){
+//         console.log("user loggedIn");
+//     }
+// }
+
+// let u1 =new Users("dhruval","hdhrghi@gmail.com")
+// let u2 =new Users("dhruvi","hdhi@gmail.com")
+// let u3 =new Users("hasrh","hdhi@gmail.com")
+
+//
