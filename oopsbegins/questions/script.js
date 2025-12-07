b06dbcd80afa9025febc86f83189808b2f36a5a9
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-07 11:18:34 +0530

    oopssqustionsssssssss

---
+++ oopsbegins/questions/script.js
@@ -0,0 +1,12 @@
+// object creattion and get the function
+
+// let user = {
+//     name: "dhruval",
+//     email: "dhruvaldalwadi1907@gmail.com",
+//     loggedIn: function () {
+//         console.log("loggin");
+
+//     }
+// }
+// user.loggedIn()
+
