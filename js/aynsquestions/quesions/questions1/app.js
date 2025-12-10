af26586f851a4e150a89d38d0ef6c68d830f0bda
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-10 11:06:12 +0530

    asyncjsquestion1done

--- js/aynsquestions/quesions/questions1/app.js
+++ js/aynsquestions/quesions/questions1/app.js
@@ -1,11 +1,13 @@
-function afterDelay(time, cb) {
-    console.log("fetching the data...");
-    setTimeout(() => {
-        cb("callback executed.....")
-    }, time);
+// 1.delay function banke uske callback me time lao ot us time settiout ko tum use karo apne settimour ke time me
+// function afterDelay(time, cb) {
 
-}
+//     console.log("fetching the data...");
+//     setTimeout(() => {
+//         cb("callback executed.....")
+//     }, time);
 
-afterDelay(2000, function (data) {
-    console.log(data);
-})
+// }
+
+// afterDelay(2000, function (data) {
+//     console.log(data);
+// })
