412db90352c28bea4f3793ff770f4879385fe392
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-10 14:25:03 +0530

    donefotheday

--- js/aynsquestions/quesions/3level/app.js
+++ js/aynsquestions/quesions/3level/app.js
@@ -1,104 +1,136 @@
-// // aapka laptop phele select karo fir us laptop ke service centre me jao konsa part badlana hai usko selcet karo fir send request karo
+// // // aapka laptop phele select karo fir us laptop ke service centre me jao konsa part badlana hai usko selcet karo fir send request karo
 
-// function laptopselcetkaro(laptop, cb) {
-//     console.log("getting the laptops...");
+// // function laptopselcetkaro(laptop, cb) {
+// //     console.log("getting the laptops...");
+// //     setTimeout(() => {
+// //         cb("[dell,lenovo,thinpad,macbook]")
+// //     }, 1000);
+// // }
+
+// // function selectkaroLaptop(name, cb) {
+// //     console.log("getting the specificee laptop info...");
+// //     setTimeout(() => {
+// //         cb("{id:10185,name:dell,model:inspiron}")
+// //     }, 2000);
+// // }
+
+// // function kyaproblemhaiShowkaro(model, cb) {
+// //     console.log("showing the problem...");
+// //     setTimeout(() => {
+// //         cb("[fan,ram,hardware,software]")
+// //     }, 3000);
+// // }
+
+// // laptopselcetkaro(laptopselcetkaro, function (laptop) {
+// //     console.log(laptop);
+// //     selectkaroLaptop(laptop.name, function (info) {
+// //         console.log(info);
+// //         kyaproblemhaiShowkaro(info.model,function(problem){
+// //             console.log(problem);
+// //         })
+// //     })
+// // })
+
+
+// // active lenahai to activa show karo sare konsa lena hai wo show karo kya selct kiya last me wo selct karo 
+
+// function vehicaleKonsalenahi(name, cb) {
+//     console.log("getting your dream bike");
 //     setTimeout(() => {
-//         cb("[dell,lenovo,thinpad,macbook]")
+//         cb({ active: "activa", bike: "bike", car: "car" })
 //     }, 1000);
 // }
 
-// function selectkaroLaptop(name, cb) {
-//     console.log("getting the specificee laptop info...");
+// function selctkaroKonsalenhai(specificename, cb) {
+//     console.log("getting your vehicle info");
 //     setTimeout(() => {
-//         cb("{id:10185,name:dell,model:inspiron}")
+//         cb({ name: "activa", g3model: 3, g4model: 4, g5model: 5, })
 //     }, 2000);
 // }
 
-// function kyaproblemhaiShowkaro(model, cb) {
-//     console.log("showing the problem...");
+// function selectionofVehicle(modelname, cb) {
+//     console.log("here's ur everything about vehicle");
 //     setTimeout(() => {
-//         cb("[fan,ram,hardware,software]")
+//         cb({ model: "3model", price: 2345123, color: "color" })
 //     }, 3000);
 // }
 
-// laptopselcetkaro(laptopselcetkaro, function (laptop) {
-//     console.log(laptop);
-//     selectkaroLaptop(laptop.name, function (info) {
-//         console.log(info);
-//         kyaproblemhaiShowkaro(info.model,function(problem){
-//             console.log(problem);
+
+// function showingcolorintoActive(color, cb) {
+//     console.log("showing all the color.....");
+//     setTimeout(() => {
+//         cb({ red: "red", black: "black", grey: "grey", blue: "blue" })
+//     }, 4000);
+// }
+
+// function choosecolorofactive(color, cb) {
+//     setTimeout(() => {
+//         cb(
+//             console.log("grey color in not available")
+//         )
+//     }, 4500);
+// }
+
+// function chooseanothercolor(chagecolor, cb) {
+//     cb(
+//         console.log("black color is possible to delivered")
+//     )
+// }
+
+// function sendingAddress(addresss, cb) {
+//     cb(`ur active will be placed at your ${addresss} `)
+// }
+
+// vehicaleKonsalenahi("vehicles", function (allvehicle) {
+//     console.log(allvehicle);
+//     selctkaroKonsalenhai(allvehicle.activa, function (activasss) {
+//         console.log(activasss);
+//         selctkaroKonsalenhai(activasss.g4model, function (mmodelinfo) {
+//             console.log(mmodelinfo);
+//             selectionofVehicle(mmodelinfo.g3model, function (alllinfo) {
+//                 console.log(alllinfo);
+//                 showingcolorintoActive(alllinfo.color, function (allcolors) {
+//                     console.log(allcolors);
+//                     choosecolorofactive(allcolors.grey, function (isavlid) {
+//                         // console.log(isavlid);
+//                         chooseanothercolor(allcolors.black, function (valid) {
+//                             // console.log(valid);
+//                             sendingAddress("anand", function (address) {
+//                                 console.log(address);
+//                             })
+//                         })
+//                     })
+//                 })
+//             })
 //         })
 //     })
 // })
 
 
-// active lenahai to activa show karo sare konsa lena hai wo show karo kya selct kiya last me wo selct karo 
-
-function vehicaleKonsalenahi(name, cb) {
-    console.log("getting your dream bike");
+function loginUser(username, cb) {
+    console.log("getting your info...");
     setTimeout(() => {
-        cb({ active: "activa", bike: "bike", car: "car" })
+        cb({ id: 1212, name: "harsh" })
     }, 1000);
 }
-
-function selctkaroKonsalenhai(specificename, cb) {
-    console.log("getting your vehicle info");
+function fetchpermisssion(data, cb) {
+    console.log("fethcing ur info...")
     setTimeout(() => {
-        cb({ name: "activa", g3model: 3, g4model: 4, g5model: 5, })
+        cb(["read", "write", "delete"])
     }, 2000);
 }
-
-function selectionofVehicle(modelname, cb) {
-    console.log("here's ur everything about vehicle");
+function loadDashboard() {
+    console.log("permisssionsss...")
     setTimeout(() => {
-        cb({ model: "3model", price: 2345123, color: "color" })
+        cb();
     }, 3000);
 }
 
 
-function showingcolorintoActive(color, cb) {
-    console.log("showing all the color.....");
-    setTimeout(() => {
-        cb({ red: "red", black: "black", grey: "grey", blue: "blue" })
-    }, 4000);
-}
-
-function choosecolorofactive(color, cb) {
-    setTimeout(() => {
-        cb(
-            console.log("grey color in not available")
-        )
-    }, 4500);
-}
-
-function chooseanothercolor(chagecolor, cb) {
-    cb(
-        console.log("black color is possible to delivered")
-    )
-}
-
-function sendingAddress(){
-    a    
-}
-
-vehicaleKonsalenahi("vehicles", function (allvehicle) {
-    console.log(allvehicle);
-    selctkaroKonsalenhai(allvehicle.activa, function (activasss) {
-        console.log(activasss);
-        selctkaroKonsalenhai(activasss.g4model, function (mmodelinfo) {
-            console.log(mmodelinfo);
-            selectionofVehicle(mmodelinfo.g3model, function (alllinfo) {
-                console.log(alllinfo);
-                showingcolorintoActive(alllinfo.color, function (allcolors) {
-                    console.log(allcolors);
-                    choosecolorofactive(allcolors.grey, function (isavlid) {
-                        console.log(isavlid);
-                        chooseanothercolor(allcolors.black, function (valid) {
-                            console.log(valid);
-                        })
-                    })
-                })
-            })
+loginUser("harsh", function (data) {
+    fetchpermisssion(data.id, function (permission) {
+        loadDashboard(permission, function (load) {
+            console.log("dashboard loaded");
         })
     })
 })
