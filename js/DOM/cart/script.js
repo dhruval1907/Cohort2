33a2c819e59e4cb587f7bdcf7215b10a7b244adb
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-11 01:40:23 +0530

    somedome

---
+++ js/DOM/cart/script.js
@@ -0,0 +1,12 @@
+let products = [
+    {
+        name:"White Chair",description:"Shoft Like Cloud",price:"15000",image:"https://plus.unsplash.com/premium_photo-1678074057896-eee996d4a23e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name:"White Chair",description:"Shoft Like Cloud",price:"15000",image:"https://plus.unsplash.com/premium_photo-1678074057896-eee996d4a23e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name:"White Chair",description:"Shoft Like Cloud",price:"15000",image:"https://plus.unsplash.com/premium_photo-1678074057896-eee996d4a23e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
+let popular = []
