c492dbaca5e25b6df1a81442f72d6bc7c0485acc
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-11 22:52:16 +0530

    asyncjspromisespractice

--- js/async-js/basics/app.js
+++ js/async-js/basics/app.js
@@ -17,22 +17,62 @@
 
 // Make a Promise that has a 50% chance to resolve and 50% chance to reject. How will you handle both cases?
 
-function timer(time){
+// function timer(time){
+//     return new Promise((resolve,reject)=>{
+//         let num = Math.floor(Math.random()*10)
+//         if(num<5){
+//             resolve(`the number is ${num} resolved `)
+//         }
+//         else{
+//             reject(`the number is ${num} rejected `)
+//         }
+//     })
+// }
+
+// timer()
+// .then(function(result){
+//     console.log(result);
+// })
+// .catch(function(error){
+//     console.log(error);
+// })
+
+// // You have to simulate downloading a file using a Promise. The Promise should resolve after a random delay. How will you test it?
+
+// function download(){
+//     return new Promise((resolve,reject)=>{
+//         let num = Math.floor(Math.random()*1000)
+//         setTimeout(() => {
+//             resolve(`the time for resolve this is ${num}`)
+//         }, num );
+
+//     })
+// }
+
+// download()
+// .then(function(result){
+//     console.log(result);
+// })
+// .catch(function(error){
+//     console.log("somethig happne wrong");
+// })
+
+// You have three asynchronous steps:
+
+// Step A returns a user ID
+
+// Step B takes that ID and returns user data
+
+// Step C takes user data and returns user settings
+// Design the Promise chain logic for this.
+
+function A(id){
     return new Promise((resolve,reject)=>{
-        let num = Math.floor(Math.random()*10)
-        if(num<=5){
-            resolve(`the number is ${num} resolved `)
-        }
-        else{
-            reject(`the number is ${num} rejected `)
-        }
+        resolve(`got the id here it is ${id}`)
+        reject(`got the id here it is ${id}`)
     })
 }
 
-timer()
-.then(function(result){
-    console.log(result);
-})
-.catch(function(error){
-    console.log(error);
+A(1212).then(function(){
+    
 })
