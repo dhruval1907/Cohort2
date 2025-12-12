f9a32f446716fd2f98f4cf1da0991e26de9623ad
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-12 22:23:39 +0530

    asyncjspromisespractice

--- js/async-js/quesions/app.js
+++ js/async-js/quesions/app.js
@@ -1,1 +1,11 @@
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`)
+
+function  getWeather(city){
+    let apikey = `1cc953bb42da005a51a8ee271cc092f6`
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`).then(raw=>raw.json())
+    .then(function(result){
+        console.log(result);
+    })
+}
+
+getWeather("london")
