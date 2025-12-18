2adabee22798a280c94654ffdc015db4a2315e2f
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-19 00:01:18 +0530

    someepicproject

--- js/some-epics/project1/script.js
+++ js/some-epics/project1/script.js
@@ -1,6 +1,31 @@
 var center = document.querySelector(".center")
 
+const btn = document.querySelector("#throttle");
+
+const throttleFunction = (func, delay) => {
+    let prev = 0;
+
+    return (...args) => {
+        let now = new Date().getTime();
+
+        if (now - prev > delay) {
+            prev = now;
+            return func(...args);
+        }
+    };
+};
+
+center.addEventListener(
+    "mousemove",
+    throttleFunction(() => {
+        console.log("button is clicked");
+    }, 500)
+);
+
+
+
 center.addEventListener("mousemove",function(dets){
-    console.log(dets.clientX);
     
 })
+
+
