f81950ade9b233d1b7c7480ec6c968199abc1d41
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-19 00:19:48 +0530

    someepicproject

--- js/some-epics/project1/script.js
+++ js/some-epics/project1/script.js
@@ -17,8 +17,25 @@ const throttleFunction = (func, delay) => {
 
 center.addEventListener(
     "mousemove",
-    throttleFunction(() => {
-        console.log("button is clicked");
+    throttleFunction((dets) => {
+        var div = document.createElement("div")
+        div.style.height = 270 +"px"
+        div.style.width = 200 +"px"
+        div.style.backgroundColor = "red"
+        div.style.position = "absolute"
+        div.style.zIndex = 2
+        div.style.left = dets.clientX + "px"
+        div.style.top = dets.clientY + "px"
+        div.style.overflow = "hidden"
+        center.style.overflow = "hidden"
+        console.log(div);
+        
+        document.body.appendChild(div)
+
+        setTimeout(() => {
+            div.remove()
+        }, 2000);
+        
     }, 500)
 );
 
