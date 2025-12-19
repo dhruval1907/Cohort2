84ee45927a50dffdce436bf82dbeb5143d7d40b8
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-19 23:05:58 +0530

    reactbasics

--- react-/basics-project/script.js
+++ react-/basics-project/script.js
@@ -1,5 +1,7 @@
 var h1 = React.createElement("h1",null,"kaise ho aap sb")
+var h2 = React.createElement("h2",{id:"nav"},"me badiya hu hai tu bta")
+
 
 var root = ReactDOM.createRoot(document.querySelector("#container"))
 
-root.render(h1)
+root.render([h1,h2])
