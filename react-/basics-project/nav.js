fd6eca0863ae166bb6fd9b22cdde90b91570ea7a
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-20 11:46:15 +0530

    explotingthereactdom

---
+++ react-/basics-project/nav.js
@@ -0,0 +1,5 @@
+const nav =()=>{
+    return React.createElement("nav",{id:"nav"},"home","about us")
+}
+
+export default nav;
---
+++ react-/basics-project/script.js
@@ -0,0 +1,3 @@
+import app from "./app.js"
+
+ReactDOM.render(React.createElement(app),document.querySelector("#container"))
